f92e0ac944ac9dd57c631bcdc950253d26d4d4b8
Sahithi Pinnamaraju <sahithi.pinnamaraju@gmail.com>
2026-06-12 13:19:14 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import "./index.css";
+
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
